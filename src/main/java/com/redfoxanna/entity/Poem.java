564f125bba0f631bf6dc72e1e15efd4bd2c42373
Anna Kessler <akessler@madisoncollege.edu>
2024-02-15 14:30:55 -0600

    Adding getters and setters for Poem

--- src/main/java/com/redfoxanna/entity/Poem.java
+++ src/main/java/com/redfoxanna/entity/Poem.java
@@ -1,14 +1,154 @@
 package com.redfoxanna.entity;
 
-import java.sql.Timestamp;
+import jakarta.persistence.*;
+import org.hibernate.annotations.GenericGenerator;
 
+import java.sql.Timestamp;
+import java.util.List;
+
+/**
+ * The type Poem.
+ */
+@Entity(name="Poem")
+@Table(name = "poem") // TODO: Create the poem table
 public class Poem {
-    private int poemId;
-    private String title;
-    private String author;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    @GenericGenerator(name = "native")
+    private int id;
+    @Column(name="poem_content")
     private String content;
+    @Column(name="poem_image")
     private String poemImage; // TODO: How handle images?
+    @Column(name = "user_id")
     private int userId;
+    @Column(name="created_at")
     private Timestamp createdAt;
-    private Timestamp updatedAt;
+    @Column(name="poem_tags")
+    private String genreTags;
+
+    /**
+     * Gets id.
+     *
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * Sets id.
+     *
+     * @param id the id
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * Gets content.
+     *
+     * @return the content
+     */
+    public String getContent() {
+        return content;
+    }
+
+    /**
+     * Sets content.
+     *
+     * @param content the content
+     */
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    /**
+     * Gets poem image.
+     *
+     * @return the poem image
+     */
+    public String getPoemImage() {
+        return poemImage;
+    }
+
+    /**
+     * Sets poem image.
+     *
+     * @param poemImage the poem image
+     */
+    public void setPoemImage(String poemImage) {
+        this.poemImage = poemImage;
+    }
+
+    /**
+     * Gets user id.
+     *
+     * @return the user id
+     */
+    public int getUserId() {
+        return userId;
+    }
+
+    /**
+     * Sets user id.
+     *
+     * @param userId the user id
+     */
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    /**
+     * Gets created at.
+     *
+     * @return the created at
+     */
+    public Timestamp getCreatedAt() {
+        return createdAt;
+    }
+
+    /**
+     * Sets created at.
+     *
+     * @param createdAt the created at
+     */
+    public void setCreatedAt(Timestamp createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    /**
+     * Gets the poem's genre tags
+     *
+     * @return the genre tags for the poem
+     */
+    public String getGenreTags() {
+        return genreTags;
+    }
+
+    /**
+     * Set the genre tags
+     *
+     * @param genreTags the genre tags
+     */
+    public void setGenreTags(String genreTags) {
+        this.genreTags = genreTags;
+    }
+
+    /**
+     * Gives all poem variables and values
+     * @return all the params
+     */
+    @Override
+    public String toString() {
+        return "Poem{" +
+                "id=" + id +
+                ", content='" + content + '\'' +
+                ", poemImage='" + poemImage + '\'' +
+                ", userId=" + userId +
+                ", createdAt=" + createdAt +
+                ", genreTags='" + genreTags + '\'' +
+                '}';
+    }
 }
+
