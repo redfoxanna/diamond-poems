d36feb3fe30def716a63375286865506756e2007
Anna Kessler <akessler@madisoncollege.edu>
2024-02-16 14:02:42 -0600

    delete and getAll unit tests passing for PoemDaoTest

--- src/test/java/com/redfoxanna/persistence/PoemDaoTest.java
+++ src/test/java/com/redfoxanna/persistence/PoemDaoTest.java
@@ -1,23 +1,29 @@
 package com.redfoxanna.persistence;
 
 import com.redfoxanna.entity.Poem;
+import com.redfoxanna.entity.User;
 import com.redfoxanna.util.Database;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class PoemDaoTest {
+    PoemDao poemDao;
 
     @BeforeEach
     void setUp() {
         Database database = Database.getInstance();
         database.runSQL("cleanDB.sql");
+        poemDao = new PoemDao();
     }
 
     @Test
     void getById() {
-        PoemDao poemDao = new PoemDao();
         Poem retrievedPoem = poemDao.getById(1);
         assertNotNull(retrievedPoem);
         assertEquals(1, retrievedPoem.getUser().getId());
@@ -29,14 +35,30 @@ class PoemDaoTest {
 
     @Test
     void insert() {
+        // get a User
+        UserDao userDao = new UserDao();
+        User user = userDao.getById(1);
+
+        // create a poem with the user on it
+        Poem poem = new Poem("This is a test poem", "poem2.jpg", Timestamp.valueOf(LocalDateTime.now()), "example, tag");
+        // insert the poem
+        int insertedPoemId = poemDao.insert(poem);
+        // retrieve the poem
+        Poem retrievedPoem = poemDao.getById(insertedPoemId);
+        // verify
+        assertNotNull(retrievedPoem);
     }
 
     @Test
     void delete() {
+        poemDao.delete(poemDao.getById(1));
+        assertNull(poemDao.getById(1));
     }
 
     @Test
     void getAll() {
+        List<Poem> poems = poemDao.getAll();
+        assertEquals(1, poems.size());
     }
 
     @Test
