e77aca8b96f7d79a37d0a829e99788efde735122
Anna Kessler <akessler@madisoncollege.edu>
2024-02-17 10:31:04 -0600

    Added servlet to forward to poem-add jsp

---
+++ src/main/java/com/redfoxanna/controller/PoemAddPageDisplayServlet.java
@@ -0,0 +1,26 @@
+package com.redfoxanna.controller;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Servlet that forwards to the Poem Add JSP Page
+ *
+ * @author redfoxanna
+ */
+@WebServlet(name = "poemAddPageDisplay",
+        urlPatterns = {"/poem-add-display"})
+public class PoemAddPageDisplayServlet extends HttpServlet {
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException{
+        String url = "/poem-add-jsp";
+        RequestDispatcher dispatcher = getServletContext().getRequestDispatcher(url);
+        dispatcher.forward(request, response);
+    }
+}
