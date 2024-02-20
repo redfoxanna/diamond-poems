e9300eeb5d174a6af33b45a29d760a5166073fb0
Anna Kessler <akessler@madisoncollege.edu>
2024-02-20 15:51:14 -0600

    Created a generic dao with getALl, delete, and getById

---
+++ src/main/java/com/redfoxanna/persistence/GenericDao.java
@@ -0,0 +1,78 @@
+package com.redfoxanna.persistence;
+
+import com.redfoxanna.entity.User;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Root;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import java.util.List;
+
+/**
+ * A generic DAO
+ * @author redfoxanna
+ */
+public class GenericDao<T> {
+    private Class<T> type;
+    private final Logger logger = LogManager.getLogger(this.getClass());
+
+    /**
+     * Instantiates a new Generic DAO
+     * @param type the entity type (User, Poem, etc.)
+     */
+    public GenericDao(Class<T> type) {
+        this.type = type;
+    }
+
+    /**
+     * Get an entity by id
+     * @param id the id to search by
+     * @return an entity
+     */
+    public <T>T getById(int id) {
+        Session session = getSession();
+        T entity = (T)session.get(type, id);
+        session.close();
+        return entity;
+    }
+
+    /**
+     * Delete an entity
+     * @param entity the entity to be deleted
+     */
+    public void delete(T entity) {
+        Session session = getSession();
+        Transaction transaction = session.beginTransaction();
+        session.delete(entity);
+        transaction.commit();
+        session.close();
+    }
+
+    /**
+     *
+     * @return the list of all entities from the query
+     */
+    public List<T> getAll() {
+        Session session = getSession();
+        CriteriaBuilder builder = session.getCriteriaBuilder();
+        CriteriaQuery<T> query = builder.createQuery(type);
+        Root<T> root = query.from(type);
+        List<T> list = session.createQuery(query).getResultList();
+        session.close();
+        return list;
+    }
+
+    /**
+     * Gets the open session
+     * @return the open session
+     */
+    private Session getSession() {
+        return SessionFactoryProvider.getSessionFactory().openSession();
+    }
+
+
+
+}
