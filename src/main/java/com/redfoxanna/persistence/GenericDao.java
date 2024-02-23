0c36d925458f35d238103ddd9be1b71deb4a30f3
Anna Kessler <akessler@madisoncollege.edu>
2024-02-22 22:14:33 -0600

    Added getByProperty methods to GenericDao

--- src/main/java/com/redfoxanna/persistence/GenericDao.java
+++ src/main/java/com/redfoxanna/persistence/GenericDao.java
@@ -1,13 +1,19 @@
 package com.redfoxanna.persistence;
 
-import com.redfoxanna.entity.User;
-import jakarta.persistence.criteria.CriteriaBuilder;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Expression;
+import jakarta.persistence.criteria.Predicate;
 import jakarta.persistence.criteria.Root;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
+import org.hibernate.query.criteria.HibernateCriteriaBuilder;
 
 import java.util.List;
 
@@ -32,7 +38,7 @@ public class GenericDao<T> {
      * @param id the id to search by
      * @return an entity
      */
-    public <T>T getById(int id) {
+    public <T> T getById(int id) {
         Session session = getSession();
         T entity = (T)session.get(type, id);
         session.close();
@@ -58,9 +64,40 @@ public class GenericDao<T> {
     public void delete(T entity) {
         Session session = getSession();
         Transaction transaction = session.beginTransaction();
-        session.delete(entity);
+        session.remove(entity);
         transaction.commit();
         session.close();
+    }
+
+    /**
+     * insert a new entity
+     * @param entity the entity to be inserted
+     */
+    //public int insert(T entity) {
+      //  int id = 0;
+        //Session session = getSession();
+        //Transaction transaction = session.beginTransaction();
+        //session.persist(entity);
+        //transaction.commit();
+        //id = (T)entity.getById();
+        //session.close();
+        //return id;
+    //}
+
+    /**
+     * Inserts the entity.
+     *
+     * @param entity entity to be inserted
+     */
+    public int insert(T entity) {
+        int id = 0;
+        Session session = getSession();
+        Transaction transaction = session.beginTransaction();
+        // TODO we are using a deprecated method here, is there a better way?
+        id = (int)session.save(entity);
+        transaction.commit();
+        session.close();
+        return id;
     }
 
     /**
@@ -69,12 +106,53 @@ public class GenericDao<T> {
      */
     public List<T> getAll() {
         Session session = getSession();
-        CriteriaBuilder builder = session.getCriteriaBuilder();
+        HibernateCriteriaBuilder builder = session.getCriteriaBuilder();
         CriteriaQuery<T> query = builder.createQuery(type);
         Root<T> root = query.from(type);
-        List<T> list = session.createQuery(query).getResultList();
+        List<T> list = session.createSelectionQuery(query).getResultList();
         session.close();
         return list;
+    }
+
+    /**
+     * Finds entities by one of its properties.
+     * sample usage: findByPropertyEqual("lastname", "Curry")
+     * @param propertyName the property name.
+     * @param value the value by which to find.
+     * @return the list of all entities found matching the criteria
+     */
+    public List<T> findByPropertyEqual(String propertyName, Object value) {
+        Session session = getSession();
+        HibernateCriteriaBuilder builder = session.getCriteriaBuilder();
+        CriteriaQuery<T> query = builder.createQuery(type);
+        Root<T> root = query.from(type);
+        query.select(root).where(builder.equal(root.get(propertyName),value));
+        List<T> items = session.createSelectionQuery( query ).getResultList();
+        session.close();
+        return items;
+    }
+
+    /**
+     * Finds entities by multiple properties.
+     * Inspired by https://stackoverflow.com/questions/11138118/really-dynamic-jpa-criteriabuilder
+     * @param propertyMap property and value pairs
+     * @return entities with properties equal to those passed in the map
+     *
+     *
+     */
+    public List<T> findByPropertyEqual(Map<String, Object> propertyMap) {
+        Session session = getSession();
+        HibernateCriteriaBuilder builder = session.getCriteriaBuilder();
+        CriteriaQuery<T> query = builder.createQuery(type);
+        Root<T> root = query.from(type);
+        List<Predicate> predicates = new ArrayList<Predicate>();
+        for (Map.Entry entry: propertyMap.entrySet()) {
+            predicates.add(builder.equal(root.get((String) entry.getKey()), entry.getValue()));
+        }
+        query.select(root).where(builder.and(predicates.toArray(new Predicate[predicates.size()])));
+        List<T> items = session.createSelectionQuery( query ).getResultList();
+        session.close();
+        return items;
     }
 
     /**
@@ -84,7 +162,4 @@ public class GenericDao<T> {
     private Session getSession() {
         return SessionFactoryProvider.getSessionFactory().openSession();
     }
-
-
-
 }
