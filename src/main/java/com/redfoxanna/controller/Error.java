1e6d4874cc89c89a0dbb62ca16e68757cd5dcc75
Anna Kessler <akessler@madisoncollege.edu>
2024-03-19 20:37:28 +0000

    Added servlet to forward to an error jsp

---
+++ src/main/java/com/redfoxanna/controller/Error.java
@@ -0,0 +1,27 @@
+package com.redfoxanna.controller;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class Error extends HttpServlet {
+    /**
+     * doGet that forwards the request/response to the
+     * error jsp
+     * @param request the request object
+     * @param response the response object
+     * @throws ServletException if a servlet error
+     * @throws IOException if an io error
+     */
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        String url = "/error.jsp";
+
+        RequestDispatcher dispatcher = getServletContext().getRequestDispatcher(url);
+        dispatcher.forward(request, response);
+    }
+}
