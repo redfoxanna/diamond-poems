fef316281249fc848edc0722546257639e1627ba
Anna Kessler <akessler@madisoncollege.edu>
2024-03-26 20:36:14 -0500

    Added some logic in logout class

--- src/main/java/com/redfoxanna/controller/LogOut.java
+++ src/main/java/com/redfoxanna/controller/LogOut.java
@@ -1,12 +1,42 @@
 package com.redfoxanna.controller;
 
+import com.redfoxanna.util.PropertiesLoader;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
 
 @WebServlet(
         urlPatterns = {"/logOut"}
 )
 
-public class LogOut extends HttpServlet {
+/** Logs a user out
+ *
+ */
+public class LogOut extends HttpServlet implements PropertiesLoader {
+
+    private final Logger logger = LogManager.getLogger(this.getClass());
+
+    /**
+     * Remove User object from session, redirect to the home page
+     * @param req servlet request
+     * @param resp servlet response
+     * @throws ServletException
+     * @throws IOException
+     */
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        HttpSession session = req.getSession();
+        logger.debug("User logged out: " + session.getAttribute("userName"));
+        session.removeAttribute("userName");
+        String url = "index.jsp";
+        resp.sendRedirect(url);
+    }
 }
