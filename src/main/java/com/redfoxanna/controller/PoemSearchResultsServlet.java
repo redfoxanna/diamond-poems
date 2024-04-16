519bceba48ea2c59e1238eff20d9af18ce4c265c
Anna Kessler <akessler@madisoncollege.edu>
2024-04-16 12:24:35 -0500

    Updated instance of dao to fix null pointer exception

--- src/main/java/com/redfoxanna/controller/PoemSearchResultsServlet.java
+++ src/main/java/com/redfoxanna/controller/PoemSearchResultsServlet.java
@@ -2,6 +2,8 @@ package com.redfoxanna.controller;
 
 import com.redfoxanna.entity.Poem;
 import com.redfoxanna.persistence.GenericDao;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.io.*;
 import javax.servlet.*;
@@ -19,11 +21,11 @@ import javax.servlet.http.*;
         urlPatterns = { "/poem-search-results" }
 )
 public class PoemSearchResultsServlet extends HttpServlet {
+    private final Logger logger = LogManager.getLogger(this.getClass());
 
     /**
-     * Gets the EmployeeDirectory instance from the ServletContext,
-     * the search term and search type and forwards the request and
-     * response to the employee-search-results JSP
+     * Gets the search term and search type and forwards the request
+     * and response to the poem-search-results JSP
      * @param request the request object
      * @param response the response object
      * @throws ServletException if a servlet error
@@ -32,8 +34,10 @@ public class PoemSearchResultsServlet extends HttpServlet {
     public void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
-        GenericDao<Poem> poemDao = new GenericDao<>();
+        GenericDao<Poem> poemDao = new GenericDao<>(Poem.class);
         request.setAttribute("poems", poemDao.getAll());
+        logger.debug("The poems: " + poemDao);
+
         String url = "/poem-search-results.jsp";
 
         RequestDispatcher dispatcher = getServletContext().getRequestDispatcher(url);
