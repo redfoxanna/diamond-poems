abd7c50acc7df83a8aa02eb6d04a6d0d86a3e401
Anna Kessler <akessler@madisoncollege.edu>
2024-04-23 16:14:04 -0500

    Added instance variable to Poem for author

--- src/main/java/com/redfoxanna/entity/Poem.java
+++ src/main/java/com/redfoxanna/entity/Poem.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import org.hibernate.annotations.GenericGenerator;
 
 import java.sql.Timestamp;
+import java.time.LocalDateTime;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Objects;
@@ -27,6 +28,8 @@ public class Poem {
     private String poemImage;
     @Column(name="created_at")
     private Timestamp createdAt;
+    @Column(name="uploaded_by")
+    private String uploadedBy;
     @OneToMany(mappedBy = "poem", fetch = FetchType.EAGER, cascade = CascadeType.REMOVE)
     private Set<PoemGenre> genres = new HashSet<>();
 
@@ -39,9 +42,11 @@ public class Poem {
     /**
      * Constructor for a poem with parameters
      */
-    public Poem(String content, String poemImage) {
+    public Poem(String content, String poemImage, String uploadedBy) {
         this.content = content;
         this.poemImage = poemImage;
+        this.createdAt = Timestamp.valueOf(LocalDateTime.now());
+        this.uploadedBy = uploadedBy;
     }
 
     /**
@@ -108,6 +113,30 @@ public class Poem {
     }
 
     /**
+     * Sets the time poem was inserted into the archives
+     * @param createdAt the timestamp with time created
+     */
+    public void setCreatedAt(Timestamp createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    /**
+     * Gets the user that upload the poem
+     * @return the username associated with the poem
+     */
+    public String getUploadedBy() {
+        return uploadedBy;
+    }
+
+    /**
+     * Sets the user that uploaded the poem
+     * @param uploadedBy the username of who uploaded the poem
+     */
+    public void setUploadedBy(String uploadedBy) {
+        this.uploadedBy = uploadedBy;
+    }
+
+    /**
      * Gets genres.
      *
      * @return the genres
@@ -132,6 +161,8 @@ public class Poem {
                 ", content='" + content + '\'' +
                 ", poemImage='" + poemImage + '\'' +
                 ", createdAt=" + createdAt +
+                ", uploadedBy='" + uploadedBy + '\'' +
+                ", genres=" + genres +
                 '}';
     }
 
@@ -140,12 +171,12 @@ public class Poem {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Poem poem = (Poem) o;
-        return id == poem.id && Objects.equals(content, poem.content) && Objects.equals(poemImage, poem.poemImage) && Objects.equals(createdAt, poem.createdAt) && Objects.equals(genres, poem.genres);
+        return id == poem.id && Objects.equals(content, poem.content) && Objects.equals(poemImage, poem.poemImage) && Objects.equals(createdAt, poem.createdAt) && Objects.equals(uploadedBy, poem.uploadedBy) && Objects.equals(genres, poem.genres);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, content, poemImage, createdAt, genres);
+        return Objects.hash(id, content, poemImage, createdAt, uploadedBy, genres);
     }
 }
 
