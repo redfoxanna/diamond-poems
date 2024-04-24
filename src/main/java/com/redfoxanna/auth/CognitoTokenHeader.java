e21307e89528804afbc4b3fff383457e504f5593
Anna Kessler <akessler@madisoncollege.edu>
2024-04-23 23:03:13 -0500

    Added javadoc to CognitoTokenHeader

--- src/main/java/com/redfoxanna/auth/CognitoTokenHeader.java
+++ src/main/java/com/redfoxanna/auth/CognitoTokenHeader.java
@@ -2,19 +2,34 @@ package com.redfoxanna.auth;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-public class CognitoTokenHeader{
+/**
+ * Represents the header section of a JSON Web Token (JWT) issued by AWS Cognito.
+ * This class provides methods to access the "kid" (Key ID) and "alg" (Algorithm)
+ * properties from the token header.
+ */
+public class CognitoTokenHeader {
 
 	@JsonProperty("kid")
-	private String kid;
+	private String kid; // Key ID
 
 	@JsonProperty("alg")
-	private String alg;
+	private String alg; // Algorithm
 
-	public String getKid(){
+	/**
+	 * Retrieves the Key ID (kid) from the token header.
+	 *
+	 * @return The Key ID (kid) of the token header.
+	 */
+	public String getKid() {
 		return kid;
 	}
 
-	public String getAlg(){
+	/**
+	 * Retrieves the algorithm (alg) from the token header.
+	 *
+	 * @return The algorithm (alg) of the token header.
+	 */
+	public String getAlg() {
 		return alg;
 	}
 }
